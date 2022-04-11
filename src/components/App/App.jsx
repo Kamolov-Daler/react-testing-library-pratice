bfff7b140995971771db9f5683de69e856ca5d87
Kamolov-Daler <daler.kamolov.19@mail.ru>
2022-04-11 14:10:33 +0500

    basic unit and integration tests

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import List from "../List";
 import Search from "../Search";
 import "./App.css";
 
-
-
-const data = ['HTML', 'CSS', 'JS', 'TS', 'React', 'Vue', 'Angular', 'NodeJS'];
+const data = ["HTML", "CSS", "JS", "TS", "React", "Vue", "Angular", "NodeJS"];
 
 function App() {
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
+  const [items, setItems] = useState(data);
 
+  useEffect(() => {
+    setItems(data.filter((item) => item.toLocaleLowerCase().includes(search.toLocaleLowerCase().trim(""))));
+  }, [search]);
   return (
     <div className="App">
-      salom
-    <Search value={search} onChange={e => setSearch(e.target.value)}>
-      Find course:
-      </Search>  
-    <List items={data}/>
+      <Search value={search} onChange={(e) => setSearch(e.target.value)}>
+        Find course:
+      </Search>
+      <List items={items} />
     </div>
   );
 }
--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,9 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  
-  expect(screen.getByRole('list')).toBeInTheDocument();
-  expect(screen.getByText('HTML')).toBeInTheDocument();
+describe("App component", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it("renders learn react link", () => {
+    expect(screen.getByRole("list")).toBeInTheDocument();
+    expect(screen.getByText("Find course:")).toBeInTheDocument();
+  });
+
+  it("typing in searchbox works", () => {
+    expect(screen.queryByDisplayValue(/React/)).toBeNull();
+
+    userEvent.type(screen.getByRole("textbox"), "React");
+
+    expect(screen.getByText(/React/)).toBeInTheDocument();
+  });
+
+  it("search filter is working", () => {
+    expect(screen.getByText("Vue")).toBeInTheDocument();
+    expect(screen.getByText("JS")).toBeInTheDocument();
+
+    userEvent.type(screen.getByRole("textbox"), "JS");
+
+    expect(screen.queryByDisplayValue(/Vue/)).toBeNull();
+    expect(screen.getByText("JS")).toBeInTheDocument();
+  });
 });
